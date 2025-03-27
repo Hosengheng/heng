14471cab5bd55adf1bca463421c61e7bddb44024
Hosengheng <boyskheng@gmail.com>
2025-03-27 18:33:43 +0700

    test project

---
+++ Etic Css/java setec/scrpt.js
@@ -0,0 +1,86 @@
+//   let score = window.prompt("please enter score: ");
+//     if(score >= 90 && score <= 100){
+//         document.write("Excellent")
+//     }else if(score >= 80){
+//         document.write("Very Good")
+//     } else if( score >= 70 ){
+//         document.write("Good")
+//     } else if( score >= 60 ){
+//         document.write("Fair")
+//     } else if( score >= 50 ){
+//         document.write(" ")
+//     } else if( score >= 0 ){
+//         document.write("Fail")
+//     } else{
+//         document.write("Invalid score")
+// var myAge = parseInt(prompt("Enter your age:", "35"));
+// switch (true) {
+//     case (myAge >= 6 && myAge <= 22):
+//         document.write(" It's time to learn");
+//         break;
+//     case (myAge >= 23 && myAge <= 56):
+//         document.write("It's time to work");
+//         break;
+//     case (myAge >= 57 && myAge <= 120):
+//         document.write("It's time to retire");
+//         break;
+//     default:
+//         console.log(myAge);
+// }
+// for(let j=1; j<=5; j++) {
+//     for(let i=1; i<=j; i++) {
+//         document.write("* ");
+//     }
+//     document.write("<br>");
+// }
+//  for(let j=1; j<=5; j--) {
+//     for(let i=1; i<=j; i++) {
+//         document.write("*");
+//     }
+//     document.write("<br>");
+// }
+    // let st = " hello  ";
+    // let st = " hello  ".trimLeft();
+    // document.write(st.length);
+    // // let st = " hello  ";
+    // // let st = " hello  ".trimLeft();
+    // // let st = " hello  ".trimRight();
+    // let st = " hello  ".trim();
+    // document.write(st.length);
+    // let st = "hello";
+    // document.write(st.charAt(1));
+    //  let st ="hello"
+    //  document.write(st)
+    
+    // let st1 = "hello ";
+    // let st2 = "everybody ";
+    // let st3 = "in ";
+    // let st4 = "class";
+
+    // document.write( st1.concat(st2, st3, st4))
+    
+    // let st = "welcome to visit Cambodia.";
+    // document.write( st.indexOf("visit"));
+
+
+
+    // let st1 = "abcd";
+    // let st2 = "abcd";
+    // document.write( st1.localeCompare(st2));
+
+        // st1 == st2   =>     0
+        // st1 > st2    =>      1
+        // st1 < st2    =>      -1
+
+
+
+        // function sum(a, b, c) {
+        //     let result = a + b + c;
+           
+        // }
+        
+        // document.write("a + "<br>);
+        // document.write("b + "<br>);
+        // document.write("c + "<br>);
+
+        // sum(10,20);
